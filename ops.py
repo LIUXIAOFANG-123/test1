4de6a0eea5991fdce00f91e53a9acf840e54b1f7
LIUXIAOFANG-123 <49577505+LIUXIAOFANG-123@users.noreply.github.com>
2019-06-23 12:06:16 +0800

    Add files via upload

---
+++ ops.py
@@ -0,0 +1,115 @@
+import math
+import numpy as np 
+import tensorflow as tf
+
+from tensorflow.python.framework import ops
+
+from utils import *
+
+try:
+  image_summary = tf.image_summary
+  scalar_summary = tf.scalar_summary
+  histogram_summary = tf.histogram_summary
+  merge_summary = tf.merge_summary
+  SummaryWriter = tf.train.SummaryWriter
+except:
+  image_summary = tf.summary.image
+  scalar_summary = tf.summary.scalar
+  histogram_summary = tf.summary.histogram
+  merge_summary = tf.summary.merge
+  SummaryWriter = tf.summary.FileWriter
+
+if "concat_v2" in dir(tf):
+  def concat(tensors, axis, *args, **kwargs):
+    return tf.concat_v2(tensors, axis, *args, **kwargs)
+else:
+  def concat(tensors, axis, *args, **kwargs):
+    return tf.concat(tensors, axis, *args, **kwargs)
+
+class batch_norm(object):
+  def __init__(self, epsilon=1e-5, momentum = 0.9, name="batch_norm"):
+    with tf.variable_scope(name):
+      self.epsilon  = epsilon
+      self.momentum = momentum
+      self.name = name
+
+  def __call__(self, x, train=True):
+    return tf.contrib.layers.batch_norm(x,
+                      decay=self.momentum, 
+                      updates_collections=None,
+                      epsilon=self.epsilon,
+                      scale=True,
+                      is_training=train,
+                      scope=self.name)
+
+def conv_cond_concat(x, y):
+  """Concatenate conditioning vector on feature map axis."""
+  x_shapes = x.get_shape()
+  y_shapes = y.get_shape()
+  return concat([
+    x, y*tf.ones([x_shapes[0], x_shapes[1], x_shapes[2], y_shapes[3]])], 3)
+
+def conv2d(input_, output_dim, 
+       k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
+       name="conv2d"):
+  with tf.variable_scope(name):
+    w = tf.get_variable('w', [k_h, k_w, input_.get_shape()[-1], output_dim],
+              initializer=tf.truncated_normal_initializer(stddev=stddev))
+    conv = tf.nn.conv2d(input_, w, strides=[1, d_h, d_w, 1], padding='SAME')
+
+    biases = tf.get_variable('biases', [output_dim], initializer=tf.constant_initializer(0.0))
+    conv = tf.reshape(tf.nn.bias_add(conv, biases), conv.get_shape())
+
+    return conv
+
+def deconv2d(input_, output_shape,            #输入4×4×512,输出8×8×256
+       k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
+       name="deconv2d", with_w=False):
+  with tf.variable_scope(name):
+    # filter : [height, width, output_channels, in_channels]
+    w = tf.get_variable('w', [k_h, k_w, output_shape[-1], input_.get_shape()[-1]],  #k_h=5, k_w=5这都是filter的尺寸，
+                                                                                    # output_shape[-1]输出通道，=256希望得到256张特征图
+                                                                                    # input_.get_shape()[-1]输入通道 =512
+              initializer=tf.random_normal_initializer(stddev=stddev))
+    
+    try:
+      deconv = tf.nn.conv2d_transpose(input_, w, output_shape=output_shape,
+                strides=[1, d_h, d_w, 1])      #d_h=2, d_w=2，步长都为2，长为2，宽为2
+
+    # Support for verisons of TensorFlow before 0.7.0
+    except AttributeError:
+      deconv = tf.nn.deconv2d(input_, w, output_shape=output_shape,
+                strides=[1, d_h, d_w, 1])
+    #b参数与输出的大小的个数一样，b=256
+    biases = tf.get_variable('biases', [output_shape[-1]], initializer=tf.constant_initializer(0.0))
+    deconv = tf.reshape(tf.nn.bias_add(deconv, biases), deconv.get_shape())
+
+    if with_w:
+      return deconv, w, biases
+    else:
+      return deconv
+     
+def lrelu(x, leak=0.2, name="lrelu"):
+  return tf.maximum(x, leak*x)
+
+
+
+#input_=100,out_size=8192,W矩阵的形状是100×8192，转化为8192维的向量
+def linear(input_, output_size, scope=None, stddev=0.02, bias_start=0.0, with_w=False): #全连接层函数，
+  shape = input_.get_shape().as_list()
+
+  with tf.variable_scope(scope or "Linear"):
+    try:
+      matrix = tf.get_variable("Matrix", [shape[1], output_size], tf.float32,
+                 tf.random_normal_initializer(stddev=stddev))
+    except ValueError as err:
+        msg = "NOTE: Usually, this is due to an issue with the image dimensions.  Did you correctly set '--crop' or '--input_height' or '--output_height'?"
+        err.args = err.args + (msg,)
+        raise
+  #偏置项b=8192
+    bias = tf.get_variable("bias", [output_size],
+      initializer=tf.constant_initializer(bias_start))
+    if with_w:
+      return tf.matmul(input_, matrix) + bias, matrix, bias
+    else:
+      return tf.matmul(input_, matrix) + bias
